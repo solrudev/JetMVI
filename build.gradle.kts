04b3ecf3d6359652f64cd073c1c310a4c5a0259f
Ilya Fomichev <fomil.music@gmail.com>
2022-11-10 03:36:09 +0700

    increase version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.*
 
 val publishGroupId by extra("io.github.solrudev")
-val publishVersion by extra("0.0.10")
+val publishVersion by extra("0.0.11")
 group = publishGroupId
 version = publishVersion
 
