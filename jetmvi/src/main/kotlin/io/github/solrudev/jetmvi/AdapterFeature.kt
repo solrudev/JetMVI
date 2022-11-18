497edac896d00d02033d7626d6b3741b8e0c48f4
Ilya Fomichev <fomil.music@gmail.com>
2022-11-18 20:50:01 +0700

    replace AdapterFeature public API with wrap Feature extensions

--- jetmvi/src/main/kotlin/io/github/solrudev/jetmvi/AdapterFeature.kt
+++ jetmvi/src/main/kotlin/io/github/solrudev/jetmvi/AdapterFeature.kt
@@ -6,23 +6,56 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 /**
- * Implementation of [Feature] which receives an [AssemblyFeature] and maps its event and UI state types using provided
- * [JetEventMapper] and [JetStateMapper].
+ * Wraps a [Feature] and maps its event and UI state types using provided mappers.
+ * @param eventMapper function which transforms from dispatched events type to feature's event type.
+ * @param stateMapper function which transforms from feature's UI state type to desired UI state
+ */
+public fun <InEvent : JetEvent, InState : JetState, OutEvent : JetEvent, OutState : JetState> Feature<InEvent, InState>.wrap(
+	eventMapper: (OutEvent) -> InEvent?,
+	stateMapper: (InState) -> OutState
+): Feature<OutEvent, OutState> {
+	return AdapterFeature(this, eventMapper, stateMapper)
+}
+
+/**
+ * Wraps a [Feature] and maps its event type using provided mapper.
+ * @param eventMapper function which transforms from dispatched events type to feature's event type.
+ */
+@JvmName("wrapEvent")
+public fun <InEvent : JetEvent, OutEvent : JetEvent, State : JetState> Feature<InEvent, State>.wrap(
+	eventMapper: (OutEvent) -> InEvent?
+): Feature<OutEvent, State> {
+	return AdapterFeature(this, eventMapper, stateMapper = { it })
+}
+
+/**
+ * Wraps a [Feature] and maps its UI state type using provided mapper.
+ * @param stateMapper function which transforms from feature's UI state type to desired UI state
+ */
+@JvmName("wrapState")
+public fun <Event : JetEvent, InState : JetState, OutState : JetState> Feature<Event, InState>.wrap(
+	stateMapper: (InState) -> OutState
+): Feature<Event, OutState> {
+	return AdapterFeature(this, eventMapper = { it }, stateMapper)
+}
+
+/**
+ * Implementation of [Feature] which receives an [Feature] and maps its event and UI state types using provided mappers.
  *
- * @param feature [AssemblyFeature] to transform.
- * @param eventMapper [JetEventMapper] which transforms from dispatched events type to [feature's][feature] event type.
- * @param stateMapper [JetStateMapper] which transforms from [feature's][feature] UI state type to desired UI state
+ * @param feature [Feature] to wrap.
+ * @param eventMapper function which transforms from dispatched events type to [feature's][feature] event type.
+ * @param stateMapper function which transforms from [feature's][feature] UI state type to desired UI state
  * type.
  */
-public open class AdapterFeature<in InEvent : JetEvent, in InState : JetState, in OutEvent : JetEvent, out OutState : JetState>(
-	private val feature: AssemblyFeature<InEvent, InState>,
-	private val eventMapper: JetEventMapper<OutEvent, InEvent?>,
-	stateMapper: JetStateMapper<InState, OutState>,
+private class AdapterFeature<in InEvent : JetEvent, in InState : JetState, in OutEvent : JetEvent, out OutState : JetState>(
+	private val feature: Feature<InEvent, InState>,
+	private val eventMapper: (OutEvent) -> InEvent?,
+	private val stateMapper: (InState) -> OutState,
 ) : Feature<OutEvent, OutState>, Flow<OutState> by feature.map(stateMapper) {
 
-	final override fun launchIn(scope: CoroutineScope): Job = feature.launchIn(scope)
+	override fun launchIn(scope: CoroutineScope): Job = feature.launchIn(scope)
 
-	final override fun dispatchEvent(event: OutEvent): Boolean {
+	override fun dispatchEvent(event: OutEvent): Boolean {
 		return eventMapper(event)?.let(feature::dispatchEvent) ?: true
 	}
 }
